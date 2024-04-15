2e5c7d221fe49880d874a6863d468658c677bab5
allenkuo07202 <allenkuo07202@fakeemail.com>
2024-04-15 22:02:54 +0800

    示範request object常用的屬性

--- app.js
+++ app.js
@@ -7,6 +7,12 @@ const app = express();
 // HTTP request常見的有4種：GET, POST, PUT, DELETE，app會根據request的種類來判斷如何應對
 // 而http request的種類已經寫在header裡面，會自動去抓到
 
+// middlewares:檢查content-type
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+// 加這個{ extended: true }，會擴展urlencoded的功能
+// 在form裡面，就可以給string以外的資料
+
 // app其實就是代表我們的伺服器，兩個參數port, callback function
 app.get("/", (req, res) => {
   res.send("歡迎來到網站首頁");
@@ -43,6 +49,35 @@ app.get("/example4", (req, res) => {
 
 app.get("/actualExample", (req, res) => {
   res.send("真正的資源在這！");
+});
+
+app.get("/fruit", (req, res) => {
+  res.send("歡迎來到水果頁面");
+});
+
+app.get("/fruit/:someFruit", (req, res) => {
+  res.send("歡迎來到" + req.params.someFruit + "頁面");
+});
+
+app.get("/example", (req, res) => {
+  res.sendFile(__dirname + "/example.html");
+});
+
+app.get("/formHandling", (req, res) => {
+  //   console.log(req.query);
+  res.send(
+    "伺服器已經收到你的表單。你所提交的資料為名稱" +
+      req.query.name +
+      "以及年齡為" +
+      req.query.age
+  );
+});
+
+app.post("/formHandling_p", (req, res) => {
+  //   console.log(req.body); // undefined
+  // ==>此時就要用2個middleware(要放在程式碼的一開始)
+  let { email, password } = req.body;
+  res.send("你的信箱是" + email);
 });
 
 // 這個一定要放在其他get之後，listen之前，不然會影響到其他path
