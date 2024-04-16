19941b38f1d68d1cee330a3e95421a2393d36f7b
allenkuo07202 <allenkuo07202@fakeemail.com>
2024-04-16 16:48:40 +0800

    示範static files如何response到client端

--- app.js
+++ app.js
@@ -3,28 +3,12 @@ const express = require("express"); // 引入從npm下載的express module，
 const app = express();
 // express其實是個function，執行express後會return一個object
 
-// console.log(app);
-// HTTP request常見的有4種：GET, POST, PUT, DELETE，app會根據request的種類來判斷如何應對
-// 而http request的種類已經寫在header裡面，會自動去抓到
-
-// middleware(自訂的)
-app.use((req, res, next) => {
-  console.log("正在經過middleware...");
-  next();
-});
-
-app.use((req, res, next) => {
-  console.log("正在經過遞2個middleware...");
-  next();
-});
+// 若沒有下一行，則style.css無法套入，圖片也無法顯示
+app.use(express.static("public"));
 
 // app其實就是代表我們的伺服器，兩個參數port, callback function
 app.get("/", (req, res) => {
-  res.send("歡迎來到網站首頁");
-});
-
-app.get("/anotherPage", (req, res) => {
-  res.send("歡迎來到另一個頁面");
+  res.sendFile(__dirname + "/index.html");
 });
 
 app.listen(3000, () => {
