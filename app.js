85ddf1048990777c8334291f660fe8d9f4c2365e
allenkuo07202 <allenkuo07202@fakeemail.com>
2024-04-17 12:46:38 +0800

    npm install ejs and rendering demo

--- app.js
+++ app.js
@@ -6,17 +6,22 @@ const app = express();
 // 若沒有下一行，則style.css無法套入，圖片也無法顯示
 app.use(express.static("public"));
 
-// app其實就是代表我們的伺服器，兩個參數port, callback function
+// 法1
+// app.get("/", (req, res) => {
+//   res.render("index.ejs"); // express會去views資料夾找index.ejs並轉成index.html後再送到客戶端
+// });
+
+// 法2
+app.set("view engine", "ejs");
 app.get("/", (req, res) => {
-  res.sendFile(__dirname + "/index.html");
+  res.render("index");
 });
 
-app.get("*", (req, res) => {
-  // res.status(404);
-  // // res.status()會自動return res object(method chaining)，所以這兩行可以寫在一起
-  // res.send("錯誤頁面...");
-  // // res.send()的status code預設是200，因為就是接受client端的請求，才會回應
-  res.status(404).send("錯誤頁面...");
+app.get("/:name", (req, res) => {
+  let { name } = req.params;
+  res.render("index", { name: name });
+  // 在JS當中，出現object屬性(ex.name)與variable(ex.name)相同的狀況，則只要寫一個(name)即可
+  // res.render("index", { name });
 });
 
 app.listen(3000, () => {
