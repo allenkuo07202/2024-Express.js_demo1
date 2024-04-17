f17832f8ee292e3bb3d5fa17d0fafbeabad65acd
allenkuo07202 <allenkuo07202@fakeemail.com>
2024-04-17 18:10:53 +0800

    示範EJS基本語法

--- app.js
+++ app.js
@@ -6,22 +6,10 @@ const app = express();
 // 若沒有下一行，則style.css無法套入，圖片也無法顯示
 app.use(express.static("public"));
 
-// 法1
-// app.get("/", (req, res) => {
-//   res.render("index.ejs"); // express會去views資料夾找index.ejs並轉成index.html後再送到客戶端
-// });
-
-// 法2
 app.set("view engine", "ejs");
 app.get("/", (req, res) => {
-  res.render("index");
-});
-
-app.get("/:name", (req, res) => {
-  let { name } = req.params;
-  res.render("index", { name: name });
-  // 在JS當中，出現object屬性(ex.name)與variable(ex.name)相同的狀況，則只要寫一個(name)即可
-  // res.render("index", { name });
+  let myString = "<h1>hello world</h1>";
+  res.render("index", { myString });
 });
 
 app.listen(3000, () => {
